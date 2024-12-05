4bebb78d012f46baa899785aa3adc470304e4c70
GodlyJaaaj <sebastien.lucas@epitech.eu>
2024-12-05 22:51:07 +0100

    feat: server config basis

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ mod server;
 mod vec2;
 mod egg;
 
-use crate::map::Map;
-use crate::vec2::Size;
+use crate::server::{Server, ServerConfig};
 use mio::Token;
 use std::error::Error;
 use std::io::Read;
@@ -17,7 +16,16 @@ use std::io::Read;
 const SERVER: Token = Token(0);
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let map = Map::new(Size::from((5, 5)));
-    println!("{}", map);
+    //let map = Map::new(Size::from((5, 5)));
+    //println!("{}", map);
+    //WIP
+    //let server_config = ServerConfig::new("127.0.0.1".to_string(),4242,
+    //                  10,
+    //                  10,
+    //                  vec!["Team1".to_string(),
+    //                       "Team2".to_string()],
+    //                  4,
+    //                  1);
+    //let mut server = Server::new(server_config);
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,56 @@
-pub struct Server {}
+use mio::{Events, Poll, net::TcpListener};
+
+pub struct ServerConfig {
+    addr: String,
+    port: u16,
+    width: u8,
+    height: u8,
+    teams: Vec<String>,
+    clients_nb: u64,
+    freq: u16,
+}
+
+impl ServerConfig {
+    pub fn new(addr: String,
+               port: u16,
+               width: u8,
+               height: u8,
+               teams: Vec<String>,
+               clients_nb: u64,
+               freq: u16) -> Self {
+        ServerConfig {
+            addr,
+            port,
+            width,
+            height,
+            teams,
+            clients_nb,
+            freq,
+        }
+    }
+}
+
+pub struct Server {
+    //config: ServerConfig,
+    ticks: u128,
+    poll: Poll,
+    events: Events,
+    tcp_listener: TcpListener
+}
+
+impl Server {
+    //pub fn new(config: ServerConfig) -> Self {
+    //    Server {
+    //        config,
+    //        ticks: 0,
+    //        poll: Poll::new().unwrap(),
+    //        events: Events::with_capacity(2048),
+    //        tcp_listener: TcpListener::bind(format!("{}:{}", config.addr, config.port).parse().unwrap()).unwrap()
+    //    }
+    //}
+
+    //pub fn run(&mut self) {
+    //    println!("Server is running");
+    //}
+}
+
