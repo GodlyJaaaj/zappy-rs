7d4d6e4bf673c1ab8598f9dcc0ca6d697e08e439
GodlyJaaaj <sebastien.lucas@epitech.eu>
2024-12-05 19:26:06 +0100

    feat: added Vec<Egg> to Cell structure + pretty print

--- src/cell.rs
+++ src/cell.rs
@@ -1,7 +1,37 @@
+use std::fmt;
+use crate::egg::Egg;
 use crate::player::Player;
 use crate::resources::Resources;
 
-struct Cell {
+#[derive(Clone)]
+pub struct Cell {
     resources: Resources,
-    players: Vec<Player>,
+    players: Vec<Box<Player>>,
+    eggs: Vec<Egg>
+}
+
+impl Cell {
+    pub fn new() -> Self {
+        Cell {
+            resources: Resources::default(),
+            players: Vec::new(),
+            eggs: Vec::new()
+        }
+    }
+}
+
+const RED: &str = "\x1b[31m";
+const GREEN: &str = "\x1b[32m";
+const RESET: &str = "\x1b[0m";
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "({},{RED}{}{RESET},{GREEN}{}{RESET})",
+            self.resources,
+            self.players.len(),
+            self.eggs.len()
+        )
+    }
 }
