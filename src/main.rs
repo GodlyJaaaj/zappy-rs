61170cc8bc1b8b48f1dad5d302d50a5a8f4f56dd
GodlyJaaaj <sebastien.lucas@epitech.eu>
2024-12-05 17:45:31 +0100

    feat: replaced tokio main by mio main

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,66 @@
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+mod cell;
+mod client;
+mod gui;
+mod map;
+mod player;
+mod resources;
+mod server;
+mod vec2;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
+use std::collections::HashMap;
+use std::error::Error;
+use std::io::Read;
+use std::time::Duration;
+
+const SERVER: Token = Token(0);
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(128);
+    let mut clients: HashMap<Token, TcpStream> = HashMap::new();
+    let mut next_token = Token(SERVER.0 + 1);
+
+    let addr = "127.0.0.1:4242".parse()?;
+    let mut server = TcpListener::bind(addr)?;
+    poll.registry()
+        .register(&mut server, SERVER, Interest::READABLE)?;
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        poll.poll(&mut events, Option::from(Duration::from_secs(0)))?;
 
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
+        for event in events.iter() {
+            match event.token() {
+                SERVER => {
+                    let (mut connection, _address) = server.accept()?;
+                    let token = next_token;
+                    next_token.0 += 1;
+                    poll.registry()
+                        .register(&mut connection, token, Interest::READABLE)?;
+                    clients.insert(token, connection);
                 }
-                println!("Received: {:?}", String::from_utf8_lossy(&buf[..n]));
+                token => {
+                    if let Some(client) = clients.get_mut(&token) {
+                        let mut buf = [0; 1024];
+                        match client.read(&mut buf) {
+                            Ok(0) => {
+                                clients.remove(&token);
+                            }
+                            Ok(n) => {
+                                println!(
+                                    "Received message from {:?}: {:?}",
+                                    token,
+                                    String::from_utf8_lossy(&buf[..n])
+                                );
+                            }
+                            Err(e) => {
+                                eprintln!("Error reading from client: {:?}", e);
+                            }
+                        }
+                    }
+                }
             }
-        });
+        }
     }
 }
