4ff6c30507e61e5e4dd402545bc170f928a4639b
Noé Lopez <noelopez@free.fr>
2024-12-09 22:17:18 +0100

    feat: better resources structure

--- src/resources.rs
+++ src/resources.rs
@@ -1,18 +1,52 @@
+use core::ops::{Index, IndexMut};
 use std::fmt;
 
-#[derive(Default, Clone, Debug)]
+#[repr(u8)]
+pub enum Resource {
+    Deraumere,
+    Linemate,
+    Mendiane,
+    Phiras,
+    Sibur,
+    Thystame,
+    Food,			// Keep this last.
+}
+
+#[derive(Default, Clone)]
 pub struct Resources {
-    deraumere: u64,
-    linemate: u64,
-    mendiane: u64,
-    phiras: u64,
-    sibur: u64,
-    thystame: u64,
-    food: u64,
+    contents: [u64; Resource::Food as usize]
+}
+
+impl Index<Resource> for Resources {
+    type Output = u64;
+
+    fn index(&self, index: Resource) -> &Self::Output {
+        return &self.contents[index as usize];
+    }
+}
+
+impl IndexMut<Resource> for Resources {
+    fn index_mut(&mut self, index: Resource) -> &mut Self::Output {
+        return &mut self.contents[index as usize];
+    }
 }
 
 impl fmt::Display for Resources {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{},{},{},{},{},{},{}", self.deraumere, self.linemate, self.mendiane, self.phiras, self.sibur, self.thystame, self.food)
+        write!(f,
+	       "{},{},{},{},{},{},{}",
+	       self[Resource::Deraumere],
+	       self[Resource::Linemate],
+	       self[Resource::Mendiane],
+	       self[Resource::Phiras],
+	       self[Resource::Sibur],
+	       self[Resource::Thystame],
+	       self[Resource::Food])
+    }
+}
+
+impl fmt::Debug for Resources {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	write!(f, "{}", self)
     }
 }
