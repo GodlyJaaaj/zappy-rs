48afd4614bbbba68c18e8d41a7ab31c1e0d9c842
Noé Lopez <noelopez@free.fr>
2024-12-12 11:25:16 +0100

    style: run cargo fmt

--- src/connection.rs
+++ src/connection.rs
@@ -1,5 +1,5 @@
+use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::net::TcpStream;
-use tokio::io::{BufReader, AsyncBufReadExt};
 
 pub struct Connection {
     stream: BufReader<TcpStream>,
@@ -8,37 +8,37 @@ pub struct Connection {
 #[derive(Debug)]
 enum ConnectionError {
     Closed,
-    IO(tokio::io::Error)
+    IO(tokio::io::Error),
 }
 
 impl Connection {
     pub fn new(socket: TcpStream) -> Self {
         Connection {
-	    stream: BufReader::new(socket)
-	}
+            stream: BufReader::new(socket),
+        }
     }
 
     pub async fn handle(&mut self) {
-	loop {
-	    if let Err(e) = self.update().await {
-		println!("End of connection: {:?}", e);
-		break;
-	    }
-	}
+        loop {
+            if let Err(e) = self.update().await {
+                println!("End of connection: {:?}", e);
+                break;
+            }
+        }
     }
 
     async fn update(&mut self) -> Result<(), ConnectionError> {
-	tokio::select! {
-	    Ok(buf) = async {
-		let mut buf = String::new();
-		match self.stream.read_line(&mut buf).await {
-		    Ok(0) => Err(ConnectionError::Closed),
-		    Err(e) => Err(ConnectionError::IO(e)),
-		    _ => Ok(buf)
-		}
-	    } => {
-	    }
-	}
-	Ok(())
+        tokio::select! {
+            Ok(buf) = async {
+            let mut buf = String::new();
+            match self.stream.read_line(&mut buf).await {
+                Ok(0) => Err(ConnectionError::Closed),
+                Err(e) => Err(ConnectionError::IO(e)),
+                _ => Ok(buf)
+            }
+            } => {
+            }
+        }
+        Ok(())
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,9 @@
 use crate::connection::Connection;
 use crate::protocol::ClientAction;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc;
 
 pub struct ServerConfig {
     addr: String,
@@ -42,12 +42,11 @@ pub struct Server {
 }
 
 #[derive(Debug)]
-pub enum ServerError {
-}
+pub enum ServerError {}
 
 impl Display for ServerError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-	todo!()
+        todo!()
     }
 }
 
@@ -55,44 +54,40 @@ impl Error for ServerError {}
 
 impl Server {
     pub fn from_config(config: ServerConfig) -> Server {
-	Server {
-	    config,
-	}
+        Server { config }
     }
 
     pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
-	let addr = format!("{}:{}", self.config.addr, self.config.port);
-	let listener = TcpListener::bind(addr).await?;
-	let (tx, rx) = mpsc::channel(32);
+        let addr = format!("{}:{}", self.config.addr, self.config.port);
+        let listener = TcpListener::bind(addr).await?;
+        let (tx, rx) = mpsc::channel(32);
 
-	tokio::spawn(async move {
-	    Self::handle_connections(listener, tx).await
-	});
-	self.process_events(rx).await;
-	Ok(())
+        tokio::spawn(async move { Self::handle_connections(listener, tx).await });
+        self.process_events(rx).await;
+        Ok(())
     }
 
     async fn handle_connections(listener: TcpListener, tx: mpsc::Sender<ClientAction>) {
-	loop {
-	    tx.send(ClientAction::Forward).await.unwrap();
+        loop {
+            tx.send(ClientAction::Forward).await.unwrap();
             // The second item contains the IP and port of the new connection.
             let (socket, _) = listener.accept().await.unwrap();
-	    let ctx = tx.clone();
-	    tokio::spawn(async move {
-		Self::process_connection(socket, ctx).await;
-	    });
-	}
+            let ctx = tx.clone();
+            tokio::spawn(async move {
+                Self::process_connection(socket, ctx).await;
+            });
+        }
     }
 
     async fn process_connection(socket: TcpStream, tx: mpsc::Sender<ClientAction>) {
-	let mut client = Connection::new(socket);
-	client.handle().await;
+        let mut client = Connection::new(socket);
+        client.handle().await;
     }
 
     async fn process_events(&mut self, mut rx: mpsc::Receiver<ClientAction>) {
-	loop {
-	    let action = rx.recv().await;
-	    println!("Processing action {:?}", action);
-	}
+        loop {
+            let action = rx.recv().await;
+            println!("Processing action {:?}", action);
+        }
     }
 }
