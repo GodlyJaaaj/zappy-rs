aec83ac782ac0f9794881bb9cd921abef79dbc4b
Sébastien LUCAS <sebastien.lucas@epitech.eu>
2025-04-06 19:23:58 +0200

    feat: default impl for Vec2 u64

--- src/vec2.rs
+++ src/vec2.rs
@@ -24,6 +24,12 @@ impl Vec2<u64> {
     }
 }
 
+impl Default for Vec2<u64> {
+    fn default() -> Self {
+        Vec2::new(0, 0)
+    }
+}
+
 impl From<(u64, u64)> for Vec2<u64> {
     /// Convert a tuple into a Vec2
     fn from((x, y): (u64, u64)) -> Self {
